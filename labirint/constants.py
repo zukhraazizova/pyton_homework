435fdc59bf42c01481e5e966d012165d38582ded
zukhraazizova <zuhralipa@gmail.comclear>
2025-11-29 01:16:25 +0300

    Полностью исправлены API-тесты + добавлен Allure

--- labirint/constants.py
+++ labirint/constants.py
@@ -1,12 +1,5 @@
 BASE_URL = "https://www.labirint.ru"
-SEARCH_BOOK = "Идиот"  # или любую другую книгу
-BOOK_ID = 918221
-BASE_URL_API= "https://www.labirint.ru"
-HTTOKEN = "qAP4TRGTdNf0VaxSVswbCIZnLKSiwRe_uOfMg-UHIDyzaz16NHDojLuvyZzh9wPICFa67F4WvQXvFVLAhM9dTpKFheNi80SRvJBoCzqckGfLzksFf7srVkhWzwLcoJrJoVA"
 
-HEADERS = {
-    "Authorization": f"Bearer {HTTOKEN}",
-    "Content-Type": "application/json"
-}
-
-BOOK_ID = 918221  # "Идиот" Ф. Достоевский
+BASKET_URL = f"{BASE_URL}/ajax/basket/"
+ADD_TO_CART_URL = f"{BASE_URL}/buy.php"
+SEARCH_URL = f"{BASE_URL}/search/"
